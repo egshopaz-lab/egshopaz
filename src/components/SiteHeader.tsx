0e822b771bfa300855c6659ce71753cab72498a5
egshopaz-lab <egshop.az@gmail.com>
2026-08-01 13:33:53 +0200

    Bust cached logo asset

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -58,7 +58,7 @@ export function SiteHeader() {
             aria-label="EG Shop"
           >
             <img
-              src="/brand/eg-logo-mark.png"
+              src="/brand/eg-logo-mark-v2.png"
               alt=""
               width={512}
               height={512}
